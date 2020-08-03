bcfd451bd67e5b7718ce2113943447563d3ad6cc
Santu Roy <roysantu2002@yahoo.com>
2020-08-03 21:24:19 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import {AppRegistry} from 'react-native';
+
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-
+console.log('getAppKeys()=',AppRegistry.getAppKeys());
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
